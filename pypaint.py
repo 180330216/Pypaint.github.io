1548ffada3c0812e6dfe9430f467199d683e5873
180330216 <85273322+180330216@users.noreply.github.com>
2021-07-24 16:36:46 +0530

    Add files via upload

---
+++ pypaint.py
@@ -0,0 +1,17 @@
+from tkinter import *
+root = Tk()
+root.title("PyPaint")
+root.geometry("500x350")
+def paint(event):
+    
+    x1, y1 = (event.x-3), (event.y-3)
+    x2, y2 = (event.x+3), (event.y+3)
+    color = "Red"
+    
+    wn.create_oval(x1, y1, x2, y2, fill=color, outline=color)
+
+wn=Canvas(root, width=500, height=350, bg='white')
+
+wn.bind('<B1-Motion>', paint)
+wn.pack()
+root.mainloop()
